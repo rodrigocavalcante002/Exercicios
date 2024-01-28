ebbb501380388d1a9df650695b83d711665e67b2
rodrigocavalcante002 <132003862+rodrigocavalcante002@users.noreply.github.com>
2024-01-28 17:16:57 -0300

    Primeiro exercício de class php

---
+++ ex001/PessoaCarro.php
@@ -0,0 +1,16 @@
+<?php
+    class pessoa
+        {
+            public string $nome;
+            public int $idade;
+        
+        }
+
+    class carro 
+    {
+        public string $vaiculo;
+        public string $vaiculo2;
+        
+    }
+    
+?>
