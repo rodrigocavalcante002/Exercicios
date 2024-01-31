b80b76105aef27d5292b1ce09c729ddc9caa1910
rodrigocavalcante002 <132003862+rodrigocavalcante002@users.noreply.github.com>
2024-01-31 19:52:06 -0300

    Create index.php

---
+++ ex003_switch_case/index.php
@@ -0,0 +1,16 @@
+<?php 
+$numero = 300;
+switch($numero){
+    case '1':
+        echo "certo";
+        break;
+    case '7':
+        echo "certo";
+        break;
+    case '300':
+        echo "certooooo!!!!";
+        break;
+    default:
+    echo "ouve algum erro";
+}
+?>
