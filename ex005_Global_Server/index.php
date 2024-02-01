8062ecd223f8d9e0494a0601095cd9bef6fff29f
rodrigocavalcante002 <132003862+rodrigocavalcante002@users.noreply.github.com>
2024-02-01 17:27:50 -0300

    ex005_Global_Server

---
+++ ex005_Global_Server/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+//$_SERVER: MOSTRA ALUMAS INFORMAÇOES DE CABÇALHOS, LOCAIS DE SCRIPT E CAMINHOS:
+echo $_SERVER ['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+?>
