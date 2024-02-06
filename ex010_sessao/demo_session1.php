0c2a2e41e23eea2caab55db44881d59ab281af76
rodrigocavalcante002 <132003862+rodrigocavalcante002@users.noreply.github.com>
2024-02-06 19:18:07 -0300

    adicionei ex010 de sessão em php

---
+++ ex010_sessao/demo_session1.php
@@ -0,0 +1,23 @@
+<?php 
+    //INICIANDO SESSÃO
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+//DEFININDO SESSÃO
+$_SESSION ["favcolor"] = "gren";
+$_SESSION ["favanimal"] = "dog";
+echo "Sessions variables are set";
+
+?>
+    
+</body>
+</html>
