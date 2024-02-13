6fd0a0bac6af2b1e096cf002129855011d6da067
rodrigocavalcante002 <132003862+rodrigocavalcante002@users.noreply.github.com>
2024-02-13 18:17:23 -0300

    adicionei novo exercício php 013 exception

---
+++ ex013_exception/index.php
@@ -0,0 +1,17 @@
+<?php 
+    function divisao($dividendo, $divisor){
+        if($divisor == 0 ){
+            throw new Exception("Divisão por zero"."<br>");
+        }
+        return $dividendo / $divisor;
+    }
+
+    // A instrução try...catch...finally pode ser usada para capturar exceções. O código no bloco final sempre será executado independentemente de uma exceção ter sido detectada. 
+    try{
+        echo divisao(5, 0);
+    }catch(Exception $e){
+        echo 'Ipossível dividir'."<br>";
+    }finally{
+        echo 'Fim do processo'."<br>";
+    }
+?>
